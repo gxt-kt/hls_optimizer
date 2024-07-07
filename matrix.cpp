b92a378d355a8fc7721925f5f07a3ef30b680582
XiaoTian <47113549+gxt-kt@users.noreply.github.com>
2024-07-08 07:35:16 +0800

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -27,22 +27,67 @@
 //	}
 //}
 
-void MatrixMultiple(ap_int<8> A[4][4], ap_int<8> B[4][4], ap_int<16> C[4][4]){
+//void MatrixMultiple(ap_int<8> A[4][4], ap_int<8> B[4][4], ap_int<16> C[4][4]){
+//	#pragma HLS INTERFACE s_axilite port=return
+//	#pragma HLS INTERFACE s_axilite port=B
+//	#pragma HLS INTERFACE s_axilite port=C
+//	#pragma HLS INTERFACE s_axilite port=A
+//	#pragma HLS ARRAY_RESHAPE variable=B complete dim=1
+//	#pragma HLS ARRAY_RESHAPE variable=A complete dim=2
+//	for(int i=0;i<4;i++)
+//		for(int j=0;j<4;j++){
+//			#pragma HLS PIPELINE II=1
+//			C[i][j]=0;
+//			// 循环乘四次，并进行相加
+//			for(int k=0;k<4;k++){
+//				C[i][j]=C[i][j]+A[i][k]*B[k][j];
+//			}
+//		}
+//}
+
+
+void MatrixMultiple(double A[100], double B[100],double C[100]){
 	#pragma HLS INTERFACE s_axilite port=return
 	#pragma HLS INTERFACE s_axilite port=B
 	#pragma HLS INTERFACE s_axilite port=C
 	#pragma HLS INTERFACE s_axilite port=A
-	#pragma HLS ARRAY_RESHAPE variable=B complete dim=1
-	#pragma HLS ARRAY_RESHAPE variable=A complete dim=2
-	for(int i=0;i<4;i++)
-		for(int j=0;j<4;j++){
-			#pragma HLS PIPELINE II=1
-			C[i][j]=0;
-			// 循环乘四次，并进行相加
-			for(int k=0;k<4;k++){
-				C[i][j]=C[i][j]+A[i][k]*B[k][j];
-			}
-		}
+//	#pragma HLS ARRAY_RESHAPE variable=B complete dim=1
+//	#pragma HLS ARRAY_RESHAPE variable=A complete dim=2
+//	double a = 1, b = 2, c = 1;
+//		  VertexCurveABC abc;
+//		  abc.parameters[0][0] = 0;
+//		  abc.parameters[1][0] = 0;
+//		  abc.parameters[2][0] = 0;
+
+	  // 构建问题
+		  Problem problem;
+
+		  // 构建100条边
+		  for (int i = 0; i < 100; i++) {
+//			verticies_[0]=&abc;
+			// problem.edge_curves[i].verticies_[0].parameters[0][0] = 0;
+			// problem.edge_curves[i].verticies_[0].parameters[1][0] = 0;
+			// problem.edge_curves[i].verticies_[0].parameters[2][0] = 0;
+			double info[1][1];
+			info[0][0] = 1;
+			problem.edge_curves[i].SetInformation(info);
+//			double x = i / 100.0;
+//			problem.edge_curves[i].x_ = x;
+//			problem.edge_curves[i].y_ = std::exp(a * x * x + b * x + c);
+			problem.edge_curves[i].x_ = A[i];
+			problem.edge_curves[i].y_ = B[i];
+			problem.edge_curves_size++;
+		  }
+
+		  	  // problem.CalculateResidual();
+		  	  // std::terminate();
+		  	  // std::cout << VAR(problem.Chi2()) << std::endl;
+		  	  // problem.MakeHessian();
+		  	  problem.Solve();
+		  	  C[0]=verticies_[0].parameters[0][0];
+		  	  C[1]=verticies_[0].parameters[1][0];
+		  	  C[2]=verticies_[0].parameters[2][0];
+//		  	  MATRIXDEBUG(abc.parameters);
 }
 
 
