0310e6d8043a6bb9aa3150b25c995a44b5664700
XiaoTian <47113549+gxt-kt@users.noreply.github.com>
2024-07-12 17:07:38 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
-#include "matrix.h"
+#include "problem.h"
+#include <exception>
 #include <iostream>
-
 
 float my_exp(float x) {
   float result = 1.0;
@@ -17,75 +17,28 @@ float my_exp(float x) {
 }
 
 int main() {
-  //	ap_int<8> a[4][4]={{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}};
-  //	ap_int<8> b[4][4]={{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}};
-  //	ap_int<16> c[4][4];
-  //	MatrixMultiple(a,b,c);
-  //	for(int i=0;i<4;i++) {
-  //		for(int j=0;j<4;j++) {
-  //			std::cout << "C["<<i<<","<<j<<"]="<<c[i][j] << "  ";
-  //		}
-  //		std::cout << std::endl;
-  //	}
+  {
+    VertexPose tmp;
+    tmp.parameters[0] = 1;
+    tmp.parameters[1] = 2;
+    tmp.parameters[2] = 3;
+    tmp.parameters[3] = 0.1;
+    tmp.parameters[4] = 0.2;
+    tmp.parameters[5] = 0.3;
+    tmp.parameters[6] = sqrt(1 - (0.01 + 0.04 + 0.09));
+    Matrix<float, 7, 1> delta;
+    delta[0] = 1;
+    delta[1] = 2;
+    delta[2] = 3;
+    delta[3] = 0.1;
+    delta[4] = 0.2;
+    delta[5] = 0.1;
+    delta[6] = sqrt(1 - (0.01 + 0.04 + 0.01));
+    tmp.Plus(delta);
+    std::cout << tmp.parameters << std::endl;
+    // std::terminate();
+  }
 
-  //	CurveFittingEdge edge;
-  //	double info[1][1];
-  //	info[0][0]=1;
-  //	edge.ComputeResidual();
-  //	edge.ComputeJacobians();
-  //	edge.SetInformation(info);
-  //	std::cout << "edge.Chi2()=" << edge.Chi2() << std::endl;;
-
-  //	Test();
-  //	TestStream();
-  //	TestMyMatrixMultiple();
-  //	TestGemm();
-  //	edge.Fun();
-  //	edge.Print();
-
-  //	  double a = 1, b = 2, c = 1;
-  //	  VertexCurveABC abc;
-  //	  abc.parameters[0][0] = 0;
-  //	  abc.parameters[1][0] = 0;
-  //	  abc.parameters[2][0] = 0;
-  //
-  //	  // 构建问题
-  //	  Problem problem;
-  //
-  //	  // 构建100条边
-  //	  for (int i = 0; i < 100; i++) {
-  //	    problem.edge_curves[i].verticies_[0]=&abc;
-  //	    // problem.edge_curves[i].verticies_[0].parameters[0][0] = 0;
-  //	    // problem.edge_curves[i].verticies_[0].parameters[1][0] = 0;
-  //	    // problem.edge_curves[i].verticies_[0].parameters[2][0] = 0;
-  //	    double info[1][1];
-  //	    info[0][0] = 1;
-  //	    problem.edge_curves[i].SetInformation(info);
-  //	    double x = i / 100.0;
-  //	    problem.edge_curves[i].x_ = x;
-  //	    problem.edge_curves[i].y_ = std::exp(a * x * x + b * x + c);
-  //	    problem.edge_curves_size++;
-  //	  }
-  //
-  //	  // problem.CalculateResidual();
-  //	  // std::terminate();
-  //	  // std::cout << VAR(problem.Chi2()) << std::endl;
-  //	  // problem.MakeHessian();
-  //	  problem.Solve();
-  //	  MATRIXDEBUG(abc.parameters);
-  //	double A[100]={};
-  //	double B[100]={};
-  //	double C[100]={};
-  //	double a = 1, b = 2, c = 1;
-  //	for(int i=0;i <100;i++) {
-  //		A[i]=i/100.0;
-  //		double x=A[i];
-  //		B[i]=my_exp(a * x * x + b * x + c);
-  //	}
-  //	MatrixMultiple(A,B,C);
-  //	for(int i=0;i<3;i++) {
-  //		std::cout << C[i]  << std::endl;
-  //	}
   {
     float A[100] = {};
     float B[100] = {};
